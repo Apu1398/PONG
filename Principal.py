ac29e93cec0f6882a53a9171edaedec57a459fce
Apu <jp.1398@hotmail.com>
2018-05-10 13:48:07 -0600

    Crea la venatana

--- Principal.py
+++ Principal.py
@@ -1,4 +1,17 @@
 import pygame,sys
 from pygame.locals import *
 
-pygame.init()
+pygame.init()                                #Inicializa python
+
+ventana = pygame.display.set_mode((400,400)) #Tamaño de la ventana
+pygame.display.set_caption("PONG")           #Titulo de la ventana
+ventana.fill(pygame.Color(0,0,0))            #Color del fondo de la ventana(negro)
+
+while True:                                  #Bucle principal
+
+    for event in pygame.event.get():         #Eventos
+        if event.type== QUIT:                     #Si se pulsa la "x"
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()                  #Actualiza la pantalla
