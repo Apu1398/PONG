b0e891c7664101a1089aa80de1f9566d83a32fc3
Apu <jp.1398@hotmail.com>
2018-05-11 15:44:13 -0600

    Crea la matriz de la interfaz

--- Juego.py
+++ Juego.py
@@ -1,22 +1,22 @@
 class juego:
-    n = 25
-    m = 40
+    n = 0
+    m = 0
     matriz=[]
 
     def __init_(self,n,m,matriz):
         self.n=n
-        self.m=m
+        self.m = m
         self.matriz=matriz
 
-    def crear_matriz(self):
-        for filas in range(self.n):
+    def crear_matriz(self,n,m):
+        for filas in range(n):
             self.matriz.append([])
-            for columnas in range(self.m):
+            for columnas in range(m):
                 self.matriz[filas].append(0)
         return self.matriz
 
-juego=juego()
-m1=juego.crear_matriz()
+juego1=juego()
+m1=juego1.crear_matriz(5,5)
 print(m1)
 
 
--- Principal.py
+++ Principal.py
@@ -8,9 +8,23 @@ ventana = pygame.display.set_mode((400,400)) #Tamaño de la ventana
 pygame.display.set_caption("PONG")           #Titulo de la ventana
 ventana.fill(pygame.Color(0,0,0))            #Color del fondo de la ventana(negro)
 
-matriz_principal = juego.crear_matriz()
+matriz_principal = juego.crear_matriz()      #Crea la matriz de control
+ALTO=15
+ANCHO=15
+NEGRO = (0,0,0)
+BLANCO = (255,255,255)
 
 while True:                                  #Bucle principal
+
+    for i in range(25):                      #Bucle que actualiza la matriz dependiendo de la matriz principal
+        color=NEGRO
+        for j in range(40):
+            if matriz_principal[i][j] == 1: #Si hay un uno el cuadro será blanco
+                color=BLANCO
+            pygame.draw.rect(ventana,color,[ALTO*j,ANCHO*i,ALTO,ANCHO],0)
+
+
+
 
     for event in pygame.event.get():         #Eventos
         if event.type== QUIT:                     #Si se pulsa la "x"
