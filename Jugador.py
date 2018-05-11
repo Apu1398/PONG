9a4c241daf3ea3ccb530dd8f06495b20c5cde9ab
RodolfoBonilla <bonilla.rodolfo34@gmail.com>
2018-05-11 13:06:03 -0600

    Definicion de atributos y metodo movimiento

--- Jugador.py
+++ Jugador.py
@@ -1,9 +1,20 @@
-import pygame
-#Clase que define el metodo jugador
 
+import pygame
+
+#Clase que define el metodo jugador
 class Jugador:
-    def __init__(self, tipoJugador):
+    def __init__(self, tam_paleta, cantidad_paleta, tipo_jugador):
         pygame.sprite.Sprite.__init__(self)
 
         "Atributos"
+
+        self.tam_paleta=tam_paleta
+        self.cantidad_paleta=cantidad_paleta
+        self.posy=0
+        self.tipo_jugador=tipo_jugador
+
+
+
+    def movimiento_humano(self):
+        pass
 
