d59f265959bad838fc605f7d501f73718549dac3
Apu <jp.1398@hotmail.com>
2018-05-12 23:38:50 -0600

    Complementa dibuja_en_pantalla

--- Principal.py
+++ Principal.py
@@ -4,16 +4,19 @@ import pygame,sys
 from pygame.locals import *
 
 pygame.init()                                #Inicializa python
-ANCHO_VENNTANA=800
-LARGO_VENTANA=600
+ANCHO_VENNTANA=600
+LARGO_VENTANA=375
 ventana = pygame.display.set_mode((ANCHO_VENNTANA,LARGO_VENTANA)) #Tamaño de la ventana
 pygame.display.set_caption("PONG")           #Titulo de la ventana
 ventana.fill(pygame.Color(0,0,0))            #Color del fondo de la ventana(negro)
 
 juego=juego()
 matriz_principal = juego.crear_matriz(25,40)      #Crea la matriz de control
-#5matriz_principal
-jugador_humano1= Jugador(40, 20, 200,True, ventana )
+
+jugador1 = Jugador(0,7,9,True)
+jugador2 = Jugador(39,7,9,False)
+matriz_principal = jugador1.dibujar_en_pantalla(matriz_principal)
+matriz_principal = jugador2.dibujar_en_pantalla(matriz_principal)
 
 ALTO=15                                          #Constantes de la matriz de la interfaz
 ANCHO=15                                         #Constantes de la matriz de la interfaz
@@ -25,20 +28,16 @@ while True:                                  #Bucle principal
     for i in range(25):                      #Bucle que actualiza la matriz dependiendo de la matriz principal
 
         for j in range(40):
-            color = NEGRO                             #El color prederteminado sera el negro
+            color = BLANCO                            #El color prederteminado sera el negro
             if matriz_principal[i][j] == 1:           #Si hay un uno el cuadro será blanco
-                color=BLANCO
+                color=NEGRO
             pygame.draw.rect(ventana,color,[ALTO*j,ANCHO*i,ALTO,ANCHO],0)
-
-
-
 
     for event in pygame.event.get():          #Eventos
         if event.type== QUIT:                 #Si se pulsa la "x"
             pygame.quit()
             sys.exit()
-        elif event.type ==MOUSEBUTTONDOWN:      #Prueba con el mosueclick
-            matriz_principal[1][0] = 1
+
 
     pygame.display.update()                  #Actualiza la pantalla
 
