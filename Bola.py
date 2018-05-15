9c79a25782eb96425f102f06f38ad5c57329e6c0
Apu <jp.1398@hotmail.com>
2018-05-15 00:20:38 -0600

    Modo una 1 vs cpu funcionando (1 paleta)

--- Bola.py
+++ Bola.py
@@ -19,19 +19,27 @@ class Bola:
 
     def moverse_bola(self, matriz):
 
-        self.pos_y += self.direccion
+        self.pos_y += self.direccion                  #Siempre le suma o le resta a la posicion y que tenga actualamente
 
-        if self.pos_y == 40:
+        if self.pos_y == 40:                          #Si la posicion es 40 la puntuacion cambia
             print("Punto")
-            matriz[self.pos_x][self.pos_y-1]=0
-            self.pos_y=20
-            self.direccion = -self.direccion
-        elif matriz[self.pos_x][self.pos_y] == 1:
-            self.direccion = -self.direccion
-            self.pos_y += self.direccion
-            matriz[self.pos_x][self.pos_y] = 1
-        else:
-            matriz[self.pos_x][(self.pos_y)-self.direccion] = 0
-            matriz[self.pos_x][self.pos_y] = 1
+            matriz[self.pos_x][self.pos_y-1]=0        #Pone en 0 la posicion en la que estaba la bola...
+            self.pos_y=20                             #Para luego ponerla en el medio
+            self.direccion = -self.direccion          #La bola iría en direccion contraria
 
-        return matriz
+        elif self.pos_y == -1:                        #Si la posicion es -1 uno la puntuacion cambia
+            print("Punto")
+            matriz[self.pos_x][self.pos_y+1]=0        #Pone en 0 la posicion en la que estaba la bola...
+            self.pos_y=20                             #Para luego ponerla en el medio
+            self.direccion = -self.direccion          #La bola iría en direccion contraria
+
+        elif matriz[self.pos_x][self.pos_y] == 1:     #Si la siguiente posicion es 1 la bola "Rebota"
+            self.direccion = -self.direccion          #Por eso se le cambia la dirección
+            self.pos_y += self.direccion              #Se aumenta o disminuye la posicion en y
+            matriz[self.pos_x][self.pos_y] = 1        #La bola se mueve para el lado contrario
+
+        else:                                                    #Si no, la bola se sigue moviendo
+            matriz[self.pos_x][(self.pos_y)-self.direccion] = 0  #Primero "apaga el cuadrante anterior"
+            matriz[self.pos_x][self.pos_y] = 1                   #"Enciende el siguiente cuadrante
+
+        return matriz                                            #Retorna la matriz
