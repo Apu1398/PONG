06aa4558b0f42820fdd53dc8aff5552858417b29
Apu <jp.1398@hotmail.com>
2018-05-15 00:51:48 -0600

    Carga el modo 1 vs cpu (dos paletas)~con movimiento

--- Jugador.py
+++ Jugador.py
@@ -20,14 +20,24 @@ class Jugador():
 
     def moverse(self,matriz,direccion):
         self.y+=direccion                                           #Direccion de movimiento
-
-        if self.y >= 1 and (self.y + self.tam_paleta) <=24:
-            for i in range(1,24):                               #Iteracion que limpia la parte de la pantalla del juagdor
-             matriz[i][self.x] = 0
-            for i in range(self.y,self.y + self.tam_paleta):    #Pone en 1 la nueva posicion de la paleta
-               matriz[i][self.x]=1
+        if self.cantidad_paletas == 1:
+            if self.y >= 1 and (self.y + self.tam_paleta) <=24:
+                for i in range(1,24):                               #Iteracion que limpia la parte de la pantalla del juagdor
+                 matriz[i][self.x] = 0
+                for i in range(self.y,self.y + self.tam_paleta):    #Pone en 1 la nueva posicion de la paleta
+                 matriz[i][self.x]=1
+            else:
+                self.y-=direccion
         else:
-            self.y-=direccion
+            if self.y >= 1 and (self.y + self.tam_paleta + 2 + self.tam_paleta ) <=24:
+                for i in range(1,24):                                                      #Limpia la parte del juagdor
+                    matriz[i][self.x] = 0
+                for i in range(self.y,self.y + self.tam_paleta):
+                    matriz[i][self.x]=1
+                for i in range(self.y + self.tam_paleta + 2, self.y + self.tam_paleta + 2 + self.tam_paleta):
+                    matriz[i][self.x] = 1
+            else:
+                self.y-=direccion
 
         return matriz
 
