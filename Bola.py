86e06ecd8f14a3560d03a94f7670c1d4f65a15b2
Apu <jp.1398@hotmail.com>
2018-05-20 21:42:10 -0600

    Arregla bug

--- Bola.py
+++ Bola.py
@@ -22,11 +22,14 @@ class Bola:
         """Metodo encargado de dibujar en la matriz la bola"""
         try:
             matriz[self.pos_x][self.pos_y] = 1
-            return matriz
+
         except IndexError:
+            if self.pos_y == 40:
+                self.direccion_columnas = -self.direccion_columnas
+                self.sonido_colision.play()
+                self.pos_y = 38
+        finally:
             return matriz
-
-
 
 
     def moverse_bola(self, matriz,tupla1, tupla2):
@@ -36,105 +39,106 @@ class Bola:
         """Metodo que que le movilidad a la bola, verifica si la bola paso los bordes
            con lo que le da puntuacion a jugador que corresponda, aqui se le indica si colisiona con
            bordes o con alguna paleta"""
-        print(self.pos_x,self.pos_y)
 
-        self.pos_y += self.direccion_columnas                             #Siempre le suma o le resta a la posicion y que tenga actualamente
-        self.pos_x += self.direccion_filas
+        if self.pos_y == -1:
+            self.pos_y=2
+            self.direccion_columnas = 1
+            self.sonido_colision.play()
+            matriz[self.pos_x][self.pos_y] = 0
 
-        if self.pos_y == 40:                                              #Si la posicion es 40 la puntuacion cambia
-            self.punto1+=1                                                #Se va sumando puntos si el jugador 1 puntua
-            self.sonido_punto.play()
-            matriz[self.pos_x-self.direccion_filas][39] = 0
-            self.pos_y=20                                                 #Pone la bola en el medio
-            self.pos_x= 12                                                #Pone la bola en el medio
-            self.direccion_columnas = -self.direccion_columnas            #La bola iría en direccion contraria
-            self.direccion_filas = 0
-        elif self.pos_y == -1:                                            #Si la posicion es -1 uno la puntuacion cambia
-            self.punto2+=1                                                #Se va sumando puntos si el jugador 2 puntua
-            self.sonido_punto.play()
-            matriz[self.pos_x - self.direccion_filas][0] = 0
-            self.pos_y = 20                                               # Pone la bola en el medio
-            self.pos_x = 12                                               # Pone la bola en el medio
-            self.direccion_columnas = -self.direccion_columnas            #La bola iría en direccion contraria
-            self.direccion_filas = 0
-
-
-        elif  self.pos_y <=40 and matriz[self.pos_x][self.pos_y] == 1:                         #Si la siguiente posicion es 1 la bola "Rebota"
-
-            #Primera paleta del jugador 1
-            if self.pos_x in range(tupla1[0][0][0],tupla1[0][0][1]) and self.pos_y == 0:
-                self.direccion_filas = -1
-                self.direccion_columnas = 1
-                self.sonido_colision.play()
-            elif self.pos_x in range(tupla1[0][1][0],tupla1[0][1][1]) and self.pos_y == 0:
-                self.direccion_filas = 0
-                self.direccion_columnas = 1
-                self.sonido_colision.play()
-            elif self.pos_x in range(tupla1[0][2][0],tupla1[0][2][1]) and self.pos_y == 0:
-                self.direccion_filas = 1
-                self.direccion_columnas = 1
-                self.sonido_colision.play()
-
-            #Segunda paleta del jugador1------------------------------------------------------------
-            elif self.pos_x in range(tupla1[1][0][0],tupla1[1][0][1]) and self.pos_y == 0:
-                self.direccion_filas = -1
-                self.direccion_columnas = 1
-                self.sonido_colision.play()
-            elif self.pos_x in range(tupla1[1][1][0],tupla1[1][1][1]) and self.pos_y == 0:
-                self.direccion_filas = 0
-                self.direccion_columnas = 1
-                self.sonido_colision.play()
-            elif self.pos_x in range(tupla1[1][2][0],tupla1[1][2][1]) and self.pos_y == 0:
-                self.direccion_filas = 1
-                self.direccion_columnas = 1
-                self.sonido_colision.play()
-            #Primera paleta del jugador 2------------------------------------------------------------
-
-            elif self.pos_x in range(tupla2[0][0][0],tupla2[0][0][1]) and self.pos_y == 39:
-                self.direccion_filas = -1
-                self.direccion_columnas = -1
-                self.sonido_colision.play()
-            elif self.pos_x in range(tupla2[0][1][0],tupla2[0][1][1]) and self.pos_y == 39:
-                self.direccion_filas = 0
-                self.direccion_columnas = -1
-                self.sonido_colision.play()
-            elif self.pos_x in range(tupla2[0][2][0],tupla2[0][2][1]) and self.pos_y == 39:
-                self.direccion_filas = 1
-                self.direccion_columnas = -1
-                self.sonido_colision.play()
-            #Segunda paleta del jugador 2
-            elif self.pos_x in range(tupla2[1][0][0],tupla2[1][0][1]) and self.pos_y == 39:
-                self.direccion_filas = -1
-                self.direccion_columnas = -1
-                self.sonido_colision.play()
-            elif self.pos_x in range(tupla2[1][1][0],tupla2[1][1][1]) and self.pos_y == 39:
-                self.direccion_filas = 0
-                self.direccion_columnas = -1
-                self.sonido_colision.play()
-            elif self.pos_x in range(tupla2[1][2][0],tupla2[1][2][1]) and self.pos_y == 39:
-                self.direccion_filas = 1
-                self.direccion_columnas = -1
-                self.sonido_colision.play()
-
-            elif self.pos_x == 0 or self.pos_x == 24:
-                self.direccion_filas = -self.direccion_filas
-                self.pos_x += self.direccion_filas
-                matriz[self.pos_x][(self.pos_y) - self.direccion_columnas] = 0
-
-            self.pos_x += self.direccion_filas
-            self.pos_y += self.direccion_columnas
-            for i in range(1, 24):  #Borra la columna1
-                matriz[i][1] = 0
-            for i in range(1, 24):  #Borra la columna1
-                matriz[i][38] = 0
         else:
-            try:
+            self.pos_y += self.direccion_columnas                             #Siempre le suma o le resta a la posicion y que tenga actualamente
+            self.pos_x += self.direccion_filas
+
+            if self.pos_y == 40:                                              #Si la posicion es 40 la puntuacion cambia
+                self.punto1+=1                                                #Se va sumando puntos si el jugador 1 puntua
+                self.sonido_punto.play()
+                matriz[self.pos_x-self.direccion_filas][39] = 0
+                self.pos_y=20                                                 #Pone la bola en el medio
+                self.pos_x= 12                                                #Pone la bola en el medio
+                self.direccion_columnas = -self.direccion_columnas            #La bola iría en direccion contraria
+                self.direccion_filas = 0
+            elif self.pos_y == -1:                                            #Si la posicion es -1 uno la puntuacion cambia
+                self.punto2+=1                                                #Se va sumando puntos si el jugador 2 puntua
+                self.sonido_punto.play()
+                matriz[self.pos_x - self.direccion_filas][0] = 0
+                self.pos_y = 20                                               # Pone la bola en el medio
+                self.pos_x = 12                                               # Pone la bola en el medio
+                self.direccion_columnas = -self.direccion_columnas            #La bola iría en direccion contraria
+                self.direccion_filas = 0
+
+
+            elif  self.pos_y <=40 and matriz[self.pos_x][self.pos_y] == 1:                         #Si la siguiente posicion es 1 la bola "Rebota"
+
+                #Primera paleta del jugador 1
+                if self.pos_x in range(tupla1[0][0][0],tupla1[0][0][1]) and self.pos_y == 0:
+                    self.direccion_filas = -1
+                    self.direccion_columnas = 1
+                    self.sonido_colision.play()
+                elif self.pos_x in range(tupla1[0][1][0],tupla1[0][1][1]) and self.pos_y == 0:
+                    self.direccion_filas = 0
+                    self.direccion_columnas = 1
+                    self.sonido_colision.play()
+                elif self.pos_x in range(tupla1[0][2][0],tupla1[0][2][1]) and self.pos_y == 0:
+                    self.direccion_filas = 1
+                    self.direccion_columnas = 1
+                    self.sonido_colision.play()
+
+                #Segunda paleta del jugador1------------------------------------------------------------
+                elif self.pos_x in range(tupla1[1][0][0],tupla1[1][0][1]) and self.pos_y == 0:
+                    self.direccion_filas = -1
+                    self.direccion_columnas = 1
+                    self.sonido_colision.play()
+                elif self.pos_x in range(tupla1[1][1][0],tupla1[1][1][1]) and self.pos_y == 0:
+                    self.direccion_filas = 0
+                    self.direccion_columnas = 1
+                    self.sonido_colision.play()
+                elif self.pos_x in range(tupla1[1][2][0],tupla1[1][2][1]) and self.pos_y == 0:
+                    self.direccion_filas = 1
+                    self.direccion_columnas = 1
+                    self.sonido_colision.play()
+                #Primera paleta del jugador 2------------------------------------------------------------
+
+                elif self.pos_x in range(tupla2[0][0][0],tupla2[0][0][1]) and self.pos_y == 39:
+                    self.direccion_filas = -1
+                    self.direccion_columnas = -1
+                    self.sonido_colision.play()
+                elif self.pos_x in range(tupla2[0][1][0],tupla2[0][1][1]) and self.pos_y == 39:
+                    self.direccion_filas = 0
+                    self.direccion_columnas = -1
+                    self.sonido_colision.play()
+                elif self.pos_x in range(tupla2[0][2][0],tupla2[0][2][1]) and self.pos_y == 39:
+                    self.direccion_filas = 1
+                    self.direccion_columnas = -1
+                    self.sonido_colision.play()
+                #Segunda paleta del jugador 2
+                elif self.pos_x in range(tupla2[1][0][0],tupla2[1][0][1]) and self.pos_y == 39:
+                    self.direccion_filas = -1
+                    self.direccion_columnas = -1
+                    self.sonido_colision.play()
+                elif self.pos_x in range(tupla2[1][1][0],tupla2[1][1][1]) and self.pos_y == 39:
+                    self.direccion_filas = 0
+                    self.direccion_columnas = -1
+                    self.sonido_colision.play()
+                elif self.pos_x in range(tupla2[1][2][0],tupla2[1][2][1]) and self.pos_y == 39:
+                    self.direccion_filas = 1
+                    self.direccion_columnas = -1
+                    self.sonido_colision.play()
+
+                elif self.pos_x == 0 or self.pos_x == 24:
+                    self.direccion_filas = -self.direccion_filas
+                    self.pos_x += self.direccion_filas
+                    matriz[self.pos_x][(self.pos_y) - self.direccion_columnas] = 0
+
+                self.pos_x += self.direccion_filas
+                self.pos_y += self.direccion_columnas
+                for i in range(1, 24):  #Borra la columna1
+                    matriz[i][1] = 0
+                for i in range(1, 24):  #Borra la columna1
+                    matriz[i][38] = 0
+            else:
                 matriz[self.pos_x-self.direccion_filas][(self.pos_y)-self.direccion_columnas] = 0  #Primero "apaga el cuadrante anterior"
-                matriz[self.pos_x][self.pos_y] = 1                                                 #"Enciende el siguiente cuadrante
-            except IndexError:
-                self.pos_y == 39
-                matriz[self.pos_x - self.direccion_filas][(self.pos_y)] = 0  # Primero "apaga el cuadrante anterior"
-                matriz[self.pos_x][self.pos_y] = 1  # "Enciende el siguiente cuadrante
+                matriz[self.pos_x][self.pos_y] = 1                                               #"Enciende el siguiente cuadrante
 
         return matriz                                                                           #Retorna la matriz
 
